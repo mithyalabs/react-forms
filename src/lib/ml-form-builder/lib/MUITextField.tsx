5fc8deec1ada25a34cdef27f9bb25d6ef6f5b31e
Aniruddha <aniganesh741@gmail.com>
2021-03-19 16:46:37 +0530

    Revert to prev state on text field because of bug in prev fix

--- src/lib/ml-form-builder/lib/MUITextField.tsx
+++ src/lib/ml-form-builder/lib/MUITextField.tsx
@@ -1,12 +1,11 @@
 import { TextFieldProps, makeStyles, Theme, createStyles, TextField } from "@material-ui/core";
 import clsx from "clsx";
-import { FormikProps } from "formik";
+import { FormikValues } from "formik";
 import { get } from "lodash";
 import React from "react";
 import { MUIReadOnly } from ".";
 import { IFieldProps } from "..";
 import { getFieldError } from "../Utils";
-
 
 export interface IProps extends IFieldProps {
 	fieldProps?: TextFieldProps;
@@ -15,7 +14,7 @@ export interface IProps extends IFieldProps {
 export const MUITextField: React.FC<IProps> = (props) => {
 	const {
 		fieldProps = {} as TextFieldProps,
-		formikProps = {} as FormikProps<any>,
+		formikProps = {} as FormikValues,
 		isReadOnly = false,
 	} = props;
 
@@ -28,7 +27,7 @@ export const MUITextField: React.FC<IProps> = (props) => {
 		helperText: fieldError || fieldProps.helperText || "",
 		onChange: formikProps.handleChange,
 		onBlur: formikProps.handleBlur,
-		value: getFieldValue(formikProps, fieldProps.name || ''),
+		value: get(formikProps, `values.${fieldProps.name}`) || "",
 		className: clsx(fieldProps.className, {
 			[classes.numberInput]: fieldProps.type === "number",
 		}),
@@ -60,10 +59,3 @@ const useStyles = makeStyles<Theme>(() =>
 		},
 	})
 );
-
-const getFieldValue = (formikProps: FormikProps<any>, name: string) => {
-	let value = get(formikProps, `values.${name}`);
-	if (value === null || value === undefined || value === false || isNaN(value))
-		return '';
-	return value;
-};
