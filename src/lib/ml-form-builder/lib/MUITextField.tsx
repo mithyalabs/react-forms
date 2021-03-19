f987f302b0e34f372f5aba0eccd503d354cbd9ef
Aniruddha <aniganesh741@gmail.com>
2021-03-19 15:33:46 +0530

    Fix bug preventing 0 from being typed in number field

--- src/lib/ml-form-builder/lib/MUITextField.tsx
+++ src/lib/ml-form-builder/lib/MUITextField.tsx
@@ -1,61 +1,69 @@
 import { TextFieldProps, makeStyles, Theme, createStyles, TextField } from "@material-ui/core";
 import clsx from "clsx";
-import { FormikValues } from "formik";
+import { FormikProps } from "formik";
 import { get } from "lodash";
 import React from "react";
 import { MUIReadOnly } from ".";
 import { IFieldProps } from "..";
 import { getFieldError } from "../Utils";
 
+
 export interface IProps extends IFieldProps {
-  fieldProps?: TextFieldProps;
+	fieldProps?: TextFieldProps;
 }
 
 export const MUITextField: React.FC<IProps> = (props) => {
-  const {
-    fieldProps = {} as TextFieldProps,
-    formikProps = {} as FormikValues,
-    isReadOnly = false,
-  } = props;
+	const {
+		fieldProps = {} as TextFieldProps,
+		formikProps = {} as FormikProps<any>,
+		isReadOnly = false,
+	} = props;
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const fieldError = getFieldError(fieldProps.name || "", formikProps);
-  const updatedProps = {
-    ...fieldProps,
-    error: !!fieldError,
-    helperText: fieldError || fieldProps.helperText || "",
-    onChange: formikProps.handleChange,
-    onBlur: formikProps.handleBlur,
-    value: get(formikProps, `values.${fieldProps.name}`) || "",
-    className: clsx(fieldProps.className, {
-      [classes.numberInput]: fieldProps.type === "number",
-    }),
-  };
-  if (isReadOnly) {
-    return (
-      <MUIReadOnly label={updatedProps.label} value={updatedProps.value} />
-    );
-  }
-  return <TextField {...updatedProps} />;
+	const fieldError = getFieldError(fieldProps.name || "", formikProps);
+	const updatedProps = {
+		...fieldProps,
+		error: !!fieldError,
+		helperText: fieldError || fieldProps.helperText || "",
+		onChange: formikProps.handleChange,
+		onBlur: formikProps.handleBlur,
+		value: getFieldValue(formikProps, fieldProps.name || ''),
+		className: clsx(fieldProps.className, {
+			[classes.numberInput]: fieldProps.type === "number",
+		}),
+	};
+	if (isReadOnly) {
+		return (
+			<MUIReadOnly label={updatedProps.label} value={updatedProps.value} />
+		);
+	}
+	return <TextField {...updatedProps} />;
 };
 
 export default MUITextField;
 
 const useStyles = makeStyles<Theme>(() =>
-  createStyles({
-    numberInput: {
-      '& input[type="number"]': {
-        "& ::-webkit-outer-spin-button": {
-          "-webkit-appearance": "none",
-          margin: 0,
-        },
-        "&::-webkit-inner-spin-button": {
-          "-webkit-appearance": "none",
-          margin: 0,
-        },
-        appearance: "textfield",
-      },
-    },
-  })
+	createStyles({
+		numberInput: {
+			'& input[type="number"]': {
+				"& ::-webkit-outer-spin-button": {
+					"-webkit-appearance": "none",
+					margin: 0,
+				},
+				"&::-webkit-inner-spin-button": {
+					"-webkit-appearance": "none",
+					margin: 0,
+				},
+				appearance: "textfield",
+			},
+		},
+	})
 );
+
+const getFieldValue = (formikProps: FormikProps<any>, name: string) => {
+	let value = get(formikProps, `values.${name}`);
+	if (value === null || value === undefined || value === false || isNaN(value))
+		return '';
+	return value;
+};
